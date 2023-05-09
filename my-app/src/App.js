433932e59ba83860c2d01ef5cbaee5c254d04ede
danio86 <d.harder@gmx.at>
2023-05-09 17:13:42 +0000

    add button statefulGreetings

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+import StatefulGreeting from "./components/StatefulGreeting";
 
 function App() {
+  return (
+    <div className="App">
+      <StatefulGreeting greeting="I am a greeting Class Component!" name="Mike" />
+    </div>
+  );
+}
+
+/* function App() {
   return (
     <div className="App">
       <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" />
     </div>
   );
-}
+} */
 
 export default App;
