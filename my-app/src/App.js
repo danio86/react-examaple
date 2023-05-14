ab7aaa2ad9b36bdd76818cebb369847336bb7d84
danio86 <>
2023-05-14 15:14:45 +0000

    add lifecycles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,13 +9,16 @@ import './App.css';
 // import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
 // import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
 // import NestingComponents from "./components/NestingComponents";
-import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
+// import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
+// import LifeCyclesCDM from "./components/LifeCyclesCDM";
+// import LifeCyclesCDU from "./components/LifeCyclesCDU";
+import LifeCyclesCWU from "./components/LifeCyclesCWU";
 
 
 function App() {
   return (
     <div className="App">
-      <MethodsAsPropsParent />
+      <LifeCyclesCWU />
     </div>
   );
 }
