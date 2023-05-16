488039130e57cb31ddc245906f180fc6a76faa84
danio86 <>
2023-05-16 08:39:19 +0000

    add uncontrooled Form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,13 +13,14 @@ import './App.css';
 // import LifeCyclesCDM from "./components/LifeCyclesCDM";
 // import LifeCyclesCDU from "./components/LifeCyclesCDU";
 // import LifeCyclesCWU from "./components/LifeCyclesCWU";
-import ControlledForm from "./components/ControlledForm";
+// import ControlledForm from "./components/ControlledForm";
+import UncontrolledForm from "./components/UncontrolledForm";
 
 
 function App() {
   return (
     <div className="App">
-      <ControlledForm />
+      <UncontrolledForm />
     </div>
   );
 }
